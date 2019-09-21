0ec916fbbe03d91ee8b4b570a0ac00a164d6be19
Christine Schwerdtfeger <christine.schwerdtfeger@gmail.com>
2019-09-21 15:15:25 -0500

    Adding preloading feature for reading the data.

--- main.go
+++ main.go
@@ -80,7 +80,7 @@ func main() {
 
 	// Read some data
 	animal := Animal{}
-	dbc = db.Preload("Dog").Preload("Cat").Where(Animal{Id: 11}).Find(&animal)
+	dbc = db.Preload("Dog").Preload("Cat").Preload("Dog.PetType").Preload("Dog.PetGuardians.Guardian").Where(Animal{Id: 11}).Find(&animal)
 	if dbc.Error != nil {
 		fmt.Println("error reading from DB", err)
 	}
@@ -92,4 +92,5 @@ func printAnimal(animal Animal) {
 	fmt.Println("Id", animal.Id)
 	fmt.Println("DogID", animal.DogId)
 	fmt.Println("Dog", animal.Dog)
+	fmt.Println("Guardians", animal.Dog.PetGuardians[0].Guardian)
 }
